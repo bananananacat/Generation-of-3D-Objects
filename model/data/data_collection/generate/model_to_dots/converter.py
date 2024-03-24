006bc6f13224b98f22b0252f8fc844b31c4fc957
Mark Levin <1234mark04@mail.ru>
2024-03-24 19:18:23 +0300

    added preprocessing for models(normalize.py) and added some funcs in class Converter

--- model/data/data_collection/generate/model_to_dots/converter.py
+++ model/data/data_collection/generate/model_to_dots/converter.py
@@ -11,6 +11,7 @@ class Converter:
         bpy.ops.object.delete(use_global=False)
         bpy.ops.wm.obj_import(filepath=file_path)
 
+
     def find_min_point(self):
         min_point = [math.inf, math.inf, math.inf]
         
@@ -23,6 +24,31 @@ class Converter:
                 min_point[2] = min(coord[2], min_point[2])
 
         return mathutils.Vector(min_point)
+
+
+    def find_max_point(self):
+        max_point = [-math.inf, -math.inf, -math.inf]
+
+        for key in bpy.data.objects.keys():
+            obj = bpy.data.objects[key]
+            coords = [(obj.matrix_world @ v.co) for v in obj.data.vertices]
+            for coord in coords:
+                max_point[0] = max(coord[0], max_point[0])
+                max_point[1] = max(coord[1], max_point[1])
+                max_point[2] = max(coord[2], max_point[2])
+
+        return mathutils.Vector(max_point)
+
+
+    def find_max_by_euclidian(self):
+        radius = 0
+        for key in bpy.data.objects.keys():
+            obj = bpy.data.objects[key]
+            coords = [(obj.matrix_world @ v.co) for v in obj.data.vertices]
+            for coord in coords:
+                radius = max(((coord[0] ** 2 + coord[1] ** 2 + coord[2] **2) ** 0.5), radius)
+    
+        return radius
 
 
     def erase_extra_objects(self, erase_list):
@@ -38,6 +64,13 @@ class Converter:
             obj.location = obj.location - delta
 
 
+    def scale_objects(self, scale):
+        bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
+        for key in bpy.data.objects.keys():
+            obj = bpy.data.objects[key]
+            obj.scale = scale
+
+
     def make_points_output(self, out_path):
         output = {'x': [], 'y': [], 'z': []}
         for key in bpy.data.objects.keys():
@@ -51,9 +84,12 @@ class Converter:
         df = df.drop_duplicates()
         df.to_csv(out_path)
 
+
     def export_obj(self, export_path):
         bpy.ops.wm.obj_export(filepath=export_path)
 
+
+# example 
 
 file_path = 'land-rover-range-rover-velar-2018-1.snapshot.5/Land-Rover_Range_Rover_Velar_First_edition_HQinterior_2018.obj'
 export_path = 'first_export.obj'
