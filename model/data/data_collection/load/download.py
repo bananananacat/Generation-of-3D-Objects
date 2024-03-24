be6a93c110038816315b80758932ee9e0f0402fe
outcast <nkhamm.spb@gmail.com>
2024-03-24 23:40:09 +0300

    Added load

---
+++ model/data/data_collection/load/download.py
@@ -0,0 +1,68 @@
+import requests
+import shutil
+import os
+
+import auth
+from .glb_to_obj import *
+
+download_url = "https://api.sketchfab.com/v3/models/{}/download"
+
+
+def _get_download_url(uid):
+    print(f"Getting download url for uid {uid}")
+    r = requests.get(
+        download_url.format(uid),
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Token {auth.__API_TOKEN}",
+        },
+    )
+
+    data = r.json()
+
+    assert r.ok, f"Failed to get url {uid}: {r.status_code} - {data}"
+
+    assert "gltf" in data, f"'gltf' field not: {data}"
+    gltf = data.get("gltf")
+
+    assert "url" in gltf, f"'url' field not found: {data}"
+    url = gltf.get("url")
+
+    assert "size" in gltf, f"'size' field not found: {data}"
+    size = gltf.get("size")
+
+    return {"url": url, "size": size}
+
+
+def download_model(model_uid, file_path, id):
+    data = _get_download_url(model_uid)
+
+    download_path = os.path.join(file_path, f"{model_uid}.zip")
+    
+    print(f"Downloading model, size {(data['size'] / (1024 * 1024)):.1f}MB")
+    with requests.get(data["url"], stream=True) as r:
+        with open(download_path, "wb") as f:
+            shutil.copyfileobj(r.raw, f)
+
+    shutil.unpack_archive(download_path, file_path, "zip")
+    os.unlink(download_path)
+
+    target_location = os.path.join(file_path, "scene.gltf")
+    return target_location
+
+
+def convert_to_obj(input_path, id):
+    print(input_path)
+    import_glb(input_path)
+    out_path = os.path.join(os.path.dirname(input_path), f"scene{id}.obj")
+    export_obj(out_path)
+    
+
+def clear_extra_files(file_path):
+    for file in os.listdir(file_path):
+        current_file_path = os.path.join(file_path, file)
+        if not current_file_path.endswith(".obj"):     
+            if os.path.isfile(current_file_path):
+                os.remove(current_file_path)
+            if os.path.isdir(current_file_path):
+                shutil.rmtree(current_file_path)
