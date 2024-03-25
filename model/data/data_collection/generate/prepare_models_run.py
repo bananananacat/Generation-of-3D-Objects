fef0de0ab07a7a8732af7744f4787c6a41297ced
outcast <nkhamm.spb@gmail.com>
2024-03-25 17:53:16 +0300

    Rename Normalizer class

--- model/data/data_collection/generate/prepare_models_run.py
+++ model/data/data_collection/generate/prepare_models_run.py
@@ -1,5 +1,5 @@
 import os
-from transform import Normilizer
+from transform import Normalizer
 
 
 models_path = "../load/store"
@@ -8,7 +8,7 @@ for file in os.listdir(models_path):
     if file.endswith(".obj"):
         file_path = os.path.join(models_path, file)
         
-        transform_model = Normilizer(file_path)
+        transform_model = Normalizer(file_path)
         
         transform_model.move_to_zero()
         transform_model.normilize_to_unit_radius()
