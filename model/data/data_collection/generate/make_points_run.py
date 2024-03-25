1f6a3eacd4d0a7415a8ff34cce731bebfbc91f66
outcast <nkhamm.spb@gmail.com>
2024-03-25 18:08:32 +0300

    Add run point generation

---
+++ model/data/data_collection/generate/make_points_run.py
@@ -0,0 +1,18 @@
+import os
+from model_to_dots import Converter
+
+
+models_path = "../load/store"
+out_path = "./points"
+
+for file in sorted(os.listdir(models_path)):
+    if file.endswith(".obj"):
+        file_path = os.path.join(models_path, file)
+        
+        filename = file.split('.')[0]
+        current_out_path = os.path.join(out_path, filename + '.csv')
+                
+        convert_model = Converter(file_path)
+        convert_model.make_points_output(current_out_path)
+
+    
