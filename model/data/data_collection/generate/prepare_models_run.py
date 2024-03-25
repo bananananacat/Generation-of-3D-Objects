47e9c21c81d5af6e4408f6ba88ae566e851cdbad
outcast <nkhamm.spb@gmail.com>
2024-03-25 17:54:27 +0300

    Rename Normalizer method

--- model/data/data_collection/generate/prepare_models_run.py
+++ model/data/data_collection/generate/prepare_models_run.py
@@ -11,6 +11,6 @@ for file in os.listdir(models_path):
         transform_model = Normalizer(file_path)
         
         transform_model.move_to_zero()
-        transform_model.normilize_to_unit_radius()
+        transform_model.normalize_to_unit_radius()
         
         transform_model.export_obj(file_path)
