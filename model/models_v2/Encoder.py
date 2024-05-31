b23043fc731d4057a739f146c83425f526499642
Mark <112201735+bananananacat@users.noreply.github.com>
2024-05-31 17:37:58 +0300

    Update Encoder.py

--- model/models_v2/Encoder.py
+++ model/models_v2/Encoder.py
@@ -1,9 +1,8 @@
-#Encoder for DenseNet
-
 class Encoder(nn.Module):
-    def __init__(self, input_size, n_filters=[32, 128, 512, 2048, 4096, 2048, 512, 128, 64]): #n_filters[-1], (n/2**n_filters >= 1)
+    def __init__(self, input_size, n_filters=[32, 128, 512, 2048, 512, 128, 64]): #n_filters[-1], (n/2**n_filters >= 1)
         super().__init__()
         n_layers = len(n_filters)
+        self.dropouts = nn.ModuleList([nn.Dropout(0.3) for i in range(n_layers)])
         self.encoder = nn.ModuleList(
             [nn.Conv1d(input_size, n_filters[i], 3, padding=1) if i == 0 else nn.Conv1d(n_filters[i - 1], n_filters[i], 3, padding=1) #k, k-1//2  padding = (kernel_size-1)/2
              for i in range(n_layers)])
@@ -11,8 +10,9 @@ class Encoder(nn.Module):
 
     def forward(self, x):
         n = x.shape[2]
-        for conv, pool in zip(self.encoder, self.pools):
+        for conv, pool, drop in zip(self.encoder, self.pools, self.dropouts):
             x = conv(x)
+            x = drop(x)
             x = pool(x)
             x = F.relu(x)
         x = torch.mean(x, dim=2)
