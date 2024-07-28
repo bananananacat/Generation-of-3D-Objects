9ce5ea198588beafce9d4b4a0250974a8e82ecd1
Mark <112201735+bananananacat@users.noreply.github.com>
2024-07-28 15:57:22 +0300

    Update DenseNet_train.py

--- model/models/densenet/DenseNet_train.py
+++ model/models/densenet/DenseNet_train.py
@@ -1,68 +1,72 @@
-# DenseNet train
-# GPU is necessary
+#DenseNet train
+save_dir = 'model'
+save_nums_dir = 'nums'
 
-def point_loss(points):
-    loss = torch.empty(points.shape[0], dtype=torch.float)
-    loss = loss.to('cuda')
-    for i in range(points.shape[0]):
-        dist = torch.cdist(points[0], points[0])
-        dist = dist[(dist <= 0.01) & (dist >= 0.00001)]
-        dist = 0.002 / dist
-        loss[i] = dist.mean()
-    loss = min(1.0, loss.mean() / 5.0)
-    return loss
-
-batch_size = 32
 upsampling_factor = 2
 input_size = 3
 
-pcl_path = '/content/extracted_files/ShapeNet_pointclouds'
-json_path = '/content/extracted_files/splits/train_models.json'
-params = {'batch_size': 32, 'shuffle': True}
+pcl_path = 'ShapeNet_pointclouds/ShapeNet_pointclouds'
+json_path = 'splits/splits/train_models.json'
+
+val_pcl_path = 'ShapeNet_pointclouds/ShapeNet_pointclouds'
+val_json_path = 'splits/splits/val_models.json'
+
+params_train = {'batch_size': 32, 'shuffle': True}
+params_val = {'batch_size': 64, 'shuffle': False}
 
 training_set = BatchGen_DenceNet(json_path, pcl_path)
-training_generator = torch.utils.data.DataLoader(training_set, **params)
+valid_set = BatchGen_DenceNet(val_json_path, val_pcl_path)
+training_generator = torch.utils.data.DataLoader(training_set, **params_train)
+valid_generator = torch.utils.data.DataLoader(valid_set, **params_val)
 
 densenet = DenseNet(input_size, upsampling_factor)
 densenet = densenet.to('cuda')
-mse = nn.MSELoss()
 
 optim = torch.optim.Adam(densenet.parameters(), lr=1e-4)
-
-counter = 0
-
 points_loss_val = 0.001
-pbar = tqdm(training_generator)
 counter_ep = 1
 counter = 0
-for i in range(8):
+
+loss_list = []
+val_list = []
+loss_dict = {}
+val_dict = {}
+
+for i in range(12):
     print(f"{counter_ep} epoch")
-    for data, target in pbar:
+    for data, target in training_generator:
         counter += 1
+        print(counter)
         optim.zero_grad()
         data = data.to('cuda')
         target = target.to('cuda')
         out = densenet(data)
         out = out.reshape(-1, 2048, 3)
-
         target = target.view(-1, 2048, 3)
-      
-        loss = kaolin.metrics.pointcloud.chamfer_distance(out, target) 
+        #print(out)
+        loss = kaolin.metrics.pointcloud.chamfer_distance(out, target)
         loss = torch.mean(loss) + points_loss_val * point_loss(out)
-      
-        pbar.set_description(f"Loss: {loss.item()}")
-        if counter % 250 == 0:
-            print(counter, loss)
+        if counter % 100 == 0:
+            print(counter, loss.item())
+            #loss_list.append(loss.item())
+            loss_dict[counter] = loss.item()
+
         loss.backward()
         optim.step()
-      
-    # saving model after every epoch
-  
-    base_path = '/content/other_drive/MyDrive/'
-    file_name = f'densenet_{counter_ep}_epoch.pt'
-    full_path = os.path.join(base_path, file_name)
-    torch.save(densenet.state_dict(), full_path)
+
+    score = val(densenet, valid_generator, len(valid_generator))
+    print(f"epoch = {counter_ep}, score =", score.item())
+    #val_list.append(score.item())
+    val_dict[counter_ep] = score.item()
+    torch.save(densenet.state_dict(), os.path.join(save_dir, f'densenet_{counter_ep}_epoch.pt'))
+
+    densenet.train()
     counter_ep += 1
-  
+
 print("end of learning")
 torch.save(densenet.state_dict(), 'densenet.pt')
+
+with open(os.path.join(save_nums, 'loss'), 'wb') as f:
+    pickle.dump(loss_dict, f)
+with open(os.path.join(save_nums, 'val'), 'wb') as f:
+    pickle.dump(loss_dict, f)
