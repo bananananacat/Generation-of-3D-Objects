d246caa12d294c482137271553e73918114173f3
Mark <112201735+bananananacat@users.noreply.github.com>
2024-09-10 18:13:36 +0300

    Update importer.py

--- model/models/densenet/importer.py
+++ model/models/densenet/importer.py
@@ -1,3 +1,6 @@
+#for this project: torch=1.12.1+cu116, torchaudio=0.12.1+cu116, torchvision=0.13.1+cu116
+import kaolin
+
 import random
 import os
 import re
@@ -5,13 +8,11 @@ import sys
 import json
 import time
 import zipfile
+import pickle
+import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#also kaolin is needed, you can install it using this comands:
-#!pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu121
-#!pip install kaolin==0.15.0 -f https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.1.1_cu121.html
-#import kaolin
 
 import torch
 import torch.nn as nn
@@ -20,8 +21,10 @@ import torch.nn.functional as F
 
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
 from PIL import Image
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.model_selection import train_test_split
+from einops import rearrange, repeat, reduce, pack, unpack
+from einops.layers.torch import Rearrange, Reduce
 from sklearn.neighbors import NearestNeighbors
-from tqdm.auto import tqdm
