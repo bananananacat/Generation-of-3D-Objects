fd62090a7ff2c7e61bae7257baca2e6685fbeaba
Mark <112201735+bananananacat@users.noreply.github.com>
2024-09-10 18:16:15 +0300

    Update get_near_points.py

--- model/models/densenet/utils/get_near_points.py
+++ model/models/densenet/utils/get_near_points.py
@@ -1,34 +1,45 @@
 # This function finds 'num_near_points' nearest points for a given point cloud. This will be used for extracting local point features.
-# If there are less then 'num_near_points' points inside a circle of radius=radius, returned ndarray will be filled with [0, 0, 0];
-# it will contain [0, 0, 0] * (num_near_points - num of points inside circle) times (for each points).
+# If there are less then 'num_near_points' points inside a circle of radius=radius, returned ndarray will be filled with point(mean coordinates);
+# it will contain mean_point * (num_near_points - num of points inside circle) times (for each points).
 
-def get_nearest_points(pcl, radius=0.5, num_near_points=10):
+def get_nearest_points(pcl, radius=0.05, num_near_points=10, device="cuda"):
     points = None
-    for step, pcl_i in enumerate(pcl):
-        nbrs = NearestNeighbors(n_neighbors=num_near_points, radius=radius, algorithm='auto').fit(pcl_i)
-        distances, indices = nbrs.radius_neighbors(pcl_i)
+
+    for step, pcl_ii in enumerate(pcl):
+        pcl_i = pcl_ii.cpu()
+        nbrs = NearestNeighbors(n_neighbors=num_near_points, radius=radius, algorithm='auto').fit(pcl_i.numpy())
+        distances, indices = nbrs.radius_neighbors(pcl_i.numpy())
+
         n = pcl_i.shape[0]
-        nearest_points_all = np.zeros((n, num_near_points, 3))
+        nearest_points_all = torch.zeros((n, num_near_points, 3))
         for i in range(n):
             nearest_points_within_radius = pcl_i[indices[i]]
-            if len(nearest_points_within_radius) >= num_near_points:
-                sorted_indices = np.argsort(distances[i])[:num_near_points]
+            num_found_points = len(nearest_points_within_radius)
+
+            if num_found_points >= num_near_points:
+                sorted_indices = torch.tensor(distances[i]).argsort()[:num_near_points]
                 nearest_points_all[i] = nearest_points_within_radius[sorted_indices]
+                #perm = torch.randperm(num_near_points)
+                #nearest_points_all[i] = nearest_points_all[i][perm]
             else:
-                nearest_points_all[i, :len(nearest_points_within_radius)] = nearest_points_within_radius
-
-        nearest_points_all = np.expand_dims(nearest_points_all, axis=0)
-
+                nearest_points_all[i, :num_found_points] = nearest_points_within_radius
+                if num_found_points > 0:
+                    mean_point = nearest_points_within_radius.mean(dim=0)
+                    nearest_points_all[i, num_found_points:] = mean_point
+                    perm = torch.randperm(num_near_points)
+                    nearest_points_all[i] = nearest_points_all[i][perm]
+        nearest_points_all = nearest_points_all.unsqueeze(0)
         if step == 0:
             points = nearest_points_all
         else:
-            points = np.concatenate((points, nearest_points_all), axis=0)
+            points = torch.cat((points, nearest_points_all), dim=0)
 
     return points
 
-# function for concatenate given tensor of nearest points (from function above) with point cloud
-def concatenate_with_pcl(pcl, nearest_points_all):
-    nearest_points_all_torch = torch.tensor(nearest_points_all, dtype=torch.float32).to('cuda')
-    nearest_points_all_flattened = nearest_points_all_torch.view(nearest_points_all_torch.shape[0], nearest_points_all_torch.shape[1], -1)
+
+def concatenate_with_pcl(pcl, nearest_points_all, device):
+    nearest_points_all_torch = nearest_points_all.to(device)
+    nearest_points_all_flattened = nearest_points_all_torch.view(nearest_points_all_torch.shape[0],
+                                                                 nearest_points_all_torch.shape[1], -1)
     result = torch.cat((pcl, nearest_points_all_flattened), dim=2)
     return result
