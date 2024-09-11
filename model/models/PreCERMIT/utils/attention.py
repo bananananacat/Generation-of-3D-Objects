ffe762ca1b33623e589d0776c45ecf469dd1466b
Mark <112201735+bananananacat@users.noreply.github.com>
2024-09-11 22:11:37 +0300

    Update attention.py

--- model/models/PreCERMIT/utils/attention.py
+++ model/models/PreCERMIT/utils/attention.py
@@ -1,3 +1,36 @@
+#for this project: torch=1.12.1+cu116, torchaudio=0.12.1+cu116, torchvision=0.13.1+cu116
+import kaolin
+
+import random
+import os
+import re
+import sys
+import json
+import time
+import zipfile
+import pickle
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+from PIL import Image
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.model_selection import train_test_split
+from einops import rearrange, repeat, reduce, pack, unpack
+from einops.layers.torch import Rearrange, Reduce
+from sklearn.neighbors import NearestNeighbors
+from transformers import get_cosine_with_hard_restarts_schedule_with_warmup
+
 def scaled_dot_product_attention(query, key, value) -> torch.Tensor:
     L, S = query.size(-2), key.size(-2)
     scale_factor = 1 / math.sqrt(query.size(-1))
@@ -15,7 +48,7 @@ class SwiGLU(nn.Module):
         x, gate = x.chunk(2, dim=-1)
         return F.silu(gate) * x
 
-
+#RoPe
 class RotaryEmbedding(nn.Module):
     def __init__(self, dim, scale_base=512, use_xpos=True):
         super().__init__()
